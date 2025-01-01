3ee5a17bfd45d6738b9bdf79cf5c27f5fdf91970
Stuart <stuart.s.frew@gmail.com>
2025-01-01 18:52:49 +0000

    new event form created, errors set on partial page

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class User::DashboardController < ApplicationController
   def show
     @user = User.first
+    @events = current_user.events
   end
 end
---
+++ app/controllers/user/events_controller.rb
@@ -0,0 +1,31 @@
+class User::EventsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @events = current_user.events
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.events.new(events_params)
+    if @event.save
+      redirect to user_event_path(@event), notice: "Event created successfully"
+    else
+      puts @event.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def events_params
+    params.expect(event: [ :name, :description ])
+  end
+end
