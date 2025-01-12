e0e9a09933100da8e7f5df3adbb8485dc8c2277e
Stuart <stuart.s.frew@gmail.com>
2025-01-12 15:14:56 +0000

    normalised and formatted result.score display on show page for event

--- app/helpers/user/results_helper.rb
+++ app/helpers/user/results_helper.rb
@@ -19,4 +19,45 @@ module User::ResultsHelper
       text_field_tag("result[value]", nil, placeholder: "Enter value", class: "form-control")
     end
   end
+
+  def normalise_time(seconds)
+    hours, remaining_seconds = seconds.divmod(3600)
+    minutes, seconds = remaining_seconds.divmod(60)
+    { hours: hours,
+      minutes: minutes,
+      seconds: seconds }
+  end
+
+  def format_time(seconds)
+    time = normalise_time(seconds)
+    "%02d:%02d:%02d" % [ time[:hours], time[:minutes], time[:seconds] ]
+  end
+
+  def format_weight(kilograms)
+    "#{kilograms}kg"
+  end
+
+  def normalise_distance(meters)
+    km, meters = meters.divmod(1000)
+    { kilometers: km,
+      meters: meters }
+  end
+
+  def format_distance(meters)
+    kilometers = meters / 1000.0
+    "%.1fkm" % kilometers
+  end
+
+  def format_result(event, score)
+    case event.units
+    when "seconds"
+      format_time(score)
+    when "kilograms"
+      format_weight(score)
+    when "meters"
+      format_distance(score)
+    else
+      score.to_s
+    end
+  end
 end
